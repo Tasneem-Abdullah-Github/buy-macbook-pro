76875c9b706f5c4e1de68e7a6be46fa490b47244
1234Tasneem1234 <12tasnimtasnim@gmail.com>
2021-08-21 17:26:20 +0600

    final changes

--- js/app.js
+++ js/app.js
@@ -1,32 +1,56 @@
-function priceFixer(priceId, cost) {
-    const productCost = document.getElementById(priceId);
-    productCost.innerText = cost;
+function setPrice(id, price) {
+    document.getElementById(id).innerText = price;
 }
-
 document.getElementById('usual-memory-btn').addEventListener('click', function () {
-    priceFixer('extra-memory', 00);
+    setPrice('extra-memory', '00');
+    updateTotal();
 });
-
 document.getElementById('16gb-memory-btn').addEventListener('click', function () {
-    priceFixer('extra-memory', 180);
+    setPrice('extra-memory', '180');
+    updateTotal();
 });
-
 document.getElementById('usual-storage-btn').addEventListener('click', function () {
-    priceFixer('extra-storage', 00);
+    setPrice('extra-storage', '00');
+    updateTotal();
 });
-
 document.getElementById('512gb-storage-btn').addEventListener('click', function () {
-    priceFixer('extra-storage', 100);
+    setPrice('extra-storage', '100');
+    updateTotal();
 });
-
 document.getElementById('1tb-storage-btn').addEventListener('click', function () {
-    priceFixer('extra-storage', 180);
+    setPrice('extra-storage', '180');
+    updateTotal();
+});
+document.getElementById('free-shiping-btn').addEventListener('click', function () {
+    setPrice('shiping-charge', '00');
+    updateTotal();
+});
+document.getElementById('charge-shiping-btn').addEventListener('click', function () {
+    setPrice('shiping-charge', '20');
+    updateTotal();
 });
 
-document.getElementById('free-shiping').addEventListener('click', function () {
-    priceFixer('shiping-charge', 00);
-});
+function updateTotal() {
+    const mainCost = document.getElementById('best-price').innerText;
+    const memoryCost = document.getElementById('extra-memory').innerText;
+    const storageCost = document.getElementById('extra-storage').innerText;
+    const shipingCost = document.getElementById('shiping-charge').innerText;
+    const total = parseInt(mainCost) + parseInt(memoryCost) + parseInt(storageCost) + parseInt(shipingCost);
+    document.getElementById('total-price').innerText = total;
+    document.getElementById('grand-total').innerText = document.getElementById('total-price').innerText;
+    document.getElementById('promo-code-apply-btn').removeAttribute('disabled');
+};
 
-document.getElementById('charge-shiping').addEventListener('click', function () {
-    priceFixer('shiping-charge', 20);
+document.getElementById('promo-code-apply-btn').addEventListener('click', function () {
+    let userPromoCode = document.getElementById('promo-code-field').value;
+    if (userPromoCode.toLowerCase() == 'stevekaku') {
+        document.getElementById('total-price').innerText = parseInt(document.getElementById('total-price').innerText) - parseInt(document.getElementById('total-price').innerText) / 5;
+        document.getElementById('grand-total').innerText = document.getElementById('total-price').innerText;
+        document.getElementById('promo-code-apply-btn').setAttribute('disabled', true);
+        document.getElementById('promo-code-field').value = '';
+        document.getElementById('error').classList.add('d-none');
+    }
+    else {
+        document.getElementById('error').classList.remove('d-none');
+    }
 });
